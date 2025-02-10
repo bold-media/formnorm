174ee2eec44c04381c7c1d1cc9732a71eed33258
“DashaADM” <annettecoolley@gmail.com>
2025-02-10 14:59:21 +0300

    again fix

--- src/modules/common/Blocks/components/ImageBlock.tsx
+++ src/modules/common/Blocks/components/ImageBlock.tsx
@@ -5,18 +5,13 @@ import Image from 'next/image'
 import React from 'react'
 
 export const ImageBlock = (props: ImageBlockType) => {
-  const { image, aspectRatio, scale, alignment } = props
+  const { image, aspectRatio, scale } = props
 
   if (!image || typeof image !== 'object' || !image?.url) return null
 
   return (
     <div
-      className={cn(
-        'not-prose relative flex',
-        alignment === 'left' && 'justify-start items-center',
-        alignment === 'centered' && 'justify-center items-center',
-        alignment === 'right' && 'justify-end items-center',
-      )}
+      className={cn('not-prose relative flex')}
       style={{
         width: `${scale}%`,
       }}
