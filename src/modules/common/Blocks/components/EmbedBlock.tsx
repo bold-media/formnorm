0ee8d70edea5df647b9ae0b8d0ebc446623643df
“DashaADM” <annettecoolley@gmail.com>
2025-05-07 13:38:01 +0300

    fixing embed

--- src/modules/common/Blocks/components/EmbedBlock.tsx
+++ src/modules/common/Blocks/components/EmbedBlock.tsx
@@ -13,13 +13,13 @@ export const EmbedBlock = (props: EmbedBlockType) => {
   if (landscapeVideo || isYouTube) {
     return (
       <div className="w-[100vw] relative left-[50%] right-[50%] -ml-[50vw] -mr-[50vw] max-w-none overflow-hidden">
-        <AspectRatio ratio={32 / 9}>
+        <div className="md:aspect-[32/9] aspect-[2/1] h-[50vh] md:h-auto">
           <div
             dangerouslySetInnerHTML={{ __html: code }}
-            className="h-full w-full [&>iframe]:h-full [&>iframe]:w-full [&>iframe]:border-0 [&>iframe]:rounded-none [&>iframe]:shadow-none [&>iframe]:scale-[2]"
+            className="h-full w-full [&>iframe]:h-full [&>iframe]:w-full [&>iframe]:border-0 [&>iframe]:rounded-none [&>iframe]:shadow-none [&>iframe]:scale-[1.3] md:[&>iframe]:scale-[2] [&>iframe]:touch-none [&>iframe]:pointer-events-none"
             suppressHydrationWarning={true}
           />
-        </AspectRatio>
+        </div>
       </div>
     )
   }
